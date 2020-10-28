9933f81f929df72e4d6874467a04c648cb019c72
Caitlyn Chau <caitlynchau2@gmail.com>
2020-10-27 20:45:22 -0700

    modified parent to check time elapsed

--- Homework 3/proc_manager2.c
+++ Homework 3/proc_manager2.c
@@ -1,6 +1,6 @@
 // Caitlyn Chau
 // CS 149 Fall 20
-// Assignment 3 Part 1
+// Assignment 3 Part 2
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -125,7 +125,7 @@ int main(int argc, char * argv[]) {
 
         // calculate elapsed runtime
         clock_gettime(CLOCK_MONOTONIC, &finish);
-        elapsed = (finish.tv_sec - (finished->starttime));
+        elapsed = finish.tv_sec - (finished->starttime);
 
         // write Finished process
         sprintf(outFileName, "%d.out", pid);
@@ -139,9 +139,10 @@ int main(int argc, char * argv[]) {
         // this should get printed in .out file
         fprintf(stdout, "Finished child %d pid of parent %d\n", pid, (int)getpid());
         fflush(stdout);
-
-        fprintf(stdout, "Finished at %ld, runtime duration %lf\n", finish.tv_sec, elapsed);
+        
+        fprintf(stdout, "Finished at %ld, runtime duration %f\n", finish.tv_sec, elapsed);
         fflush(stdout);
+
 
         if (WIFEXITED(status)) {
             fprintf(stderr, "Child %d terminated normally with exit code: %d\n",
@@ -151,7 +152,62 @@ int main(int argc, char * argv[]) {
                     pid, WTERMSIG(status));
         }
 
+        if (elapsed > 2) { 
+            // restart the process
+            pid = fork();
+
+            // restart the timer
+            clock_gettime(CLOCK_MONOTONIC, &start);
+            finished->starttime = start.tv_sec;
+
+            if (pid < 0) {
+                fprintf(stderr, "fork failed");
+                exit(1);
+            }
+
+            // fork successful:
+            if (pid == 0) { // child process
+
+                sprintf(outFileName, "%d.out", (int)getpid());
+                sprintf(errFileName, "%d.err", (int)getpid());
+                int out_fd = open(outFileName, O_CREAT|O_APPEND|O_RDWR, 0777);
+                int err_fd = open(errFileName, O_CREAT|O_APPEND|O_RDWR, 0777);
+
+                // redirect output
+                dup2(out_fd, STDOUT_FILENO); // redirect stdout (1) to out_fd
+                dup2(err_fd, STDERR_FILENO); // redirect stderr (2) to err_fdc
+
+                fprintf(stdout, "RESTARTING\n");
+
+                // write Starting process
+                fprintf(stdout, "Starting command %d: child %d pid of parent %d\n",
+                        finished->index, (int)getpid(), (int)getppid());
+                fflush(stdout);
+
+                // execute command
+                char *myargs[20];
+                int index = 0;
+                while (*(current->command[index]) != '\0') {
+                    myargs[index] = current->command[index];
+                    index += 1;
+                }
+                myargs[index] = NULL;
+                execvp(myargs[0], myargs);
+
+                // print error if execvp fails
+                perror(myargs[0]);
+                exit(2);
+            } else { // parent process
+                finished->PID = pid;
+            }
+
+        } else if (elapsed < 2) {
+            // print Spawning too fast in .err file
+            fprintf(stderr, "Spawning too fast\n");
+        }   
+
         close(out_fd);
         close(err_fd);
-    }
+
+    } // end while - parent
 }
